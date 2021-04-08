a3591eacb41f8444d13ae34a870597090bb60a8a
Andy Kozlov <doesnotexists@doesnotexists.org>
2021-04-08 06:48:59 +0300

    some naming refactoring

--- DomainByAssessment.Console/Program.cs
+++ DomainByAssessment.Console/Program.cs
@@ -15,43 +15,43 @@ namespace DomainByAssessment.Console
         {
             var sources = new List<(string, string)>() { ("INTERFAX", "https://interfax.by/news/feed/"), ("HABR", "https://habr.com/ru/rss/all/all/") };
 
-            var providerUrlFeeds = new ConcurrentBag<(string, string, SyndicationFeed)>();
+            var provider_Url_Feeds = new ConcurrentBag<(string, string, SyndicationFeed)>();
 
             // I've got obvious knowledge gaps of AggregateException handling, so skipping it
             sources.AsParallel().ForAll(p =>
             {
                 using var reader = XmlReader.Create(p.Item2);
                 var feed = SyndicationFeed.Load(reader);
-                providerUrlFeeds.Add((p.Item1, p.Item2, feed));
+                provider_Url_Feeds.Add((p.Item1, p.Item2, feed));
             });
 
             using var ctx = new RssAggregatorContextFactory().CreateDbContext(Array.Empty<string>());
             ctx.Database.EnsureCreated();
 
             //ensure feed providers are added to the db
-            if (providerUrlFeeds.Select(f => f.Item1).Except(ctx.Feeds.Select(f => f.ProviderName)).Any())
+            if (provider_Url_Feeds.Select(f => f.Item1).Except(ctx.Feeds.Select(f => f.ProviderName)).Any())
             {
                 //TODO:fix adding all the feeds but filter out only missing ones in db
-                ctx.Feeds.AddRange(providerUrlFeeds.Select(f => new RssFeed() { ProviderName = f.Item1, Title = f.Item3.Title.Text }));
+                ctx.Feeds.AddRange(provider_Url_Feeds.Select(f => new RssFeed() { ProviderName = f.Item1, Title = f.Item3.Title.Text }));
                 ctx.SaveChanges();
             }
 
-            foreach (var feed in providerUrlFeeds)
+            foreach (var provider_Url_Feed in provider_Url_Feeds)
             {
                 //TODO: optimize to avoid fetching all the items but query on the db side instead
-                var freshNewsItemsSummaries = feed.Item3.Items.Select(i => i.Summary.Text).Except(ctx.NewsItems.Where(i => i.Feed.ProviderName == feed.Item1).Select(i => i.Summary));
-                ctx.NewsItems.AddRange(feed.Item3.Items
+                var freshNewsItemsSummaries = provider_Url_Feed.Item3.Items.Select(i => i.Summary.Text).Except(ctx.NewsItems.Where(i => i.Feed.ProviderName == provider_Url_Feed.Item1).Select(i => i.Summary));
+                ctx.NewsItems.AddRange(provider_Url_Feed.Item3.Items
                     .Where(i => freshNewsItemsSummaries.Contains(i.Summary.Text))
                     .Select(i => new NewsItem() {
-                        Feed = ctx.Feeds.Single(f => f.Title == feed.Item3.Title.Text),
+                        Feed = ctx.Feeds.Single(f => f.Title == provider_Url_Feed.Item3.Title.Text),
                         Guid = Guid.NewGuid(),
                         ExternalUrlId = i.Id,
                         Title = i.Title.Text,
                         PublishDateTimeOffset = i.PublishDate,
                         Summary = i.Summary.Text
                     }));
-                System.Console.WriteLine($"News received count for {feed.Item1}: {feed.Item3.Items.Count()};");
-                System.Console.WriteLine($"News saved count for {feed.Item1}: {ctx.SaveChanges()};");
+                System.Console.WriteLine($"News received count for {provider_Url_Feed.Item1}: {provider_Url_Feed.Item3.Items.Count()};");
+                System.Console.WriteLine($"News saved count for {provider_Url_Feed.Item1}: {ctx.SaveChanges()};");
             }
         }
     }
